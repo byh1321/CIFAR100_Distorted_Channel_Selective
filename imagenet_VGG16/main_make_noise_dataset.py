707f87790e15e15195385988e26f0d51c308fd02
byh1321 <byh1321@naver.com>
2018-08-17 11:58:17 +0900

    Slow dirty set making

--- imagenet_VGG16/main_make_noise_dataset.py
+++ imagenet_VGG16/main_make_noise_dataset.py
@@ -24,6 +24,7 @@ import argparse
 
 from PIL import Image
 import glob
+#from sklearn.preprocessing import normalize
 
 use_cuda = torch.cuda.is_available()
 
@@ -41,27 +42,47 @@ dirty = f.readlines()
 f.close()
 
 def imshow(img,folder_idx, img_count):
-	npimg = np.array(img)
-	#print(npimg.shape)
+	npimg = np.array(img, dtype=float)
+	npimg[:,:,0] = (npimg[:,:,0] - np.mean(npimg[:,:,0]))/2/np.std(npimg[:,:,0])+0.5
+	npimg[:,:,1] = (npimg[:,:,1] - np.mean(npimg[:,:,1]))/2/np.std(npimg[:,:,1])+0.5
+	npimg[:,:,2] = (npimg[:,:,2] - np.mean(npimg[:,:,2]))/2/np.std(npimg[:,:,2])+0.5
+
 	npimg_size = npimg.size
 	npimg_shape = npimg.shape
 
 	if args.blur_std == 0:
-		gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
 
-		npimg[0] = npimg[0] + gaussian_noise_0.reshape(npimg_shape[1],npimg_shape[2])
-		npimg[1] = npimg[1] + gaussian_noise_1.reshape(npimg_shape[1],npimg_shape[2])
-		npimg[2] = npimg[2] + gaussian_noise_2.reshape(npimg_shape[1],npimg_shape[2])
+		'''
+		try:
+			gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
+		except:
+			print(img_count)
+			print('npimg_size : ' + str(npimg_size))
+			print('npimg_shape : ' + str(npimg_shape))
+			exit()
+		'''
+		'''
+		gaussian_noise_0 = np.random.normal(0,args.gaussian_std * np.std(npimg[:,:,0]),npimg_shape[1]*npimg_shape[0])
+		gaussian_noise_1 = np.random.normal(0,args.gaussian_std * np.std(npimg[:,:,1]),npimg_shape[1]*npimg_shape[0])
+		gaussian_noise_2 = np.random.normal(0,args.gaussian_std * np.std(npimg[:,:,2]),npimg_shape[1]*npimg_shape[0])
+		npimg[:,:,0] = npimg[:,:,0] + gaussian_noise_0.reshape(npimg_shape[0],npimg_shape[1])
+		npimg[:,:,1] = npimg[:,:,1] + gaussian_noise_1.reshape(npimg_shape[0],npimg_shape[1])
+		npimg[:,:,2] = npimg[:,:,2] + gaussian_noise_2.reshape(npimg_shape[0],npimg_shape[1])
+		'''
+		gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[0])
+		gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[0])
+		gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[0])
+		npimg[:,:,0] = npimg[:,:,0] + gaussian_noise_0.reshape(npimg_shape[0],npimg_shape[1])
+		npimg[:,:,1] = npimg[:,:,1] + gaussian_noise_1.reshape(npimg_shape[0],npimg_shape[1])
+		npimg[:,:,2] = npimg[:,:,2] + gaussian_noise_2.reshape(npimg_shape[0],npimg_shape[1])
 	elif args.gaussian_std == 0:
-		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std)
-		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std)
-		npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std)
+		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std * np.std(npimg[0]))
+		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std * np.std(npimg[1]))
+		npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std * np.std(npimg[2]))
 	else:	
-		gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
+		gaussian_noise_0 = np.random.normal(0,args.gaussian_std * np.std(npimg[0]),npimg_shape[1]*npimg_shape[2])
+		gaussian_noise_1 = np.random.normal(0,args.gaussian_std * np.std(npimg[1]),npimg_shape[1]*npimg_shape[2])
+		gaussian_noise_2 = np.random.normal(0,args.gaussian_std * np.std(npimg[2]),npimg_shape[1]*npimg_shape[2])
 
 		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std) + gaussian_noise_0.reshape(npimg_shape[1],npimg_shape[2])
 		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std) + gaussian_noise_1.reshape(npimg_shape[1],npimg_shape[2])
@@ -79,8 +100,9 @@ def imshow(img,folder_idx, img_count):
 		assert os.path.isdir(foldername), 'Error: no checkpoint directory found!'
 	scipy.misc.toimage(npimg).save(foldername+"/imagenet_gaussian_{}_blur_{}_train_{i}.png".format(args.gaussian_std,args.blur_std,i=img_count))
 	
-
+f = open('dirty_image_failed.log','a+')
 print(time.ctime())
+print(time.ctime(), file=f)
 for folder_idx in range(0,1000):
 	
 	image_loader = []
@@ -94,8 +116,16 @@ for folder_idx in range(0,1000):
 	
 	img_count = 0
 	for img in image_loader:
-		imshow(img, folder_idx, img_count)
+		try:
+			imshow(img, folder_idx, img_count)
+		except:
+			print('{}th folder, {}th img skipped'.format(folder_idx, img_count))
+			print('{}th folder, {}th img skipped'.format(folder_idx, img_count), file = f)
 		img_count += 1
+		'''
+		if img_count == 1:
+			exit()
+		'''
 	progress_bar(folder_idx, 1000);
-
+f.close()
 print('\n')
--- imagenet_VGG16/old_main_make_noise_dataset.py
+++ imagenet_VGG16/old_main_make_noise_dataset.py
@@ -2,9 +2,7 @@
 some parts of code are extracted from "https://github.com/kuangliu/pytorch-cifar"
 I modified some parts for our experiment
 """
-
 from __future__ import print_function
-
 import torch
 import torchvision
 import torchvision.datasets as dset
@@ -13,96 +11,84 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 from utils import progress_bar
 import torchvision.datasets as datasets
-
 import scipy.misc
 from scipy import ndimage
-
 import os
 import numpy as np
 import argparse
-
 use_cuda = torch.cuda.is_available()
-
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--gaussian_std', default=0, type=float, help='std of gaussian noise')
 parser.add_argument('--blur_std', default=0, type=float, help='std of blur noise')
-
+parser.add_argument('--val', default=0, type=float, help='make test dataset if test=1')
+parser.add_argument('--train', default=0, type=float, help='make train dataset if train=1')
 args = parser.parse_args()
-
 transform_train = transforms.Compose([transforms.ToTensor(),
 					  transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5, 0.5, 0.5])])
 transform_test = transforms.Compose([transforms.ToTensor(),
 					 transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5, 0.5, 0.5])])
-
 #cifar_train = dset.CIFAR100("./", train=True, transform=transform_train, target_transform=None, download=True)
 #cifar_test = dset.CIFAR100("./", train=False, transform=transform_test, target_transform=None, download=True)
-
-f = open('foldernames.txt','r')
-clean = f.readlines()
-f.close()
-f = open('dirty_foldernames.txt','r')
-dirty = f.readlines()
-f.close()
-
-def imshow(img,batch_idx,folder_idx):
+traindir = os.path.join('/home/yhbyun/Imagenet2010/','train')
+valdir = os.path.join('/home/mhha/','val')
+train_dataset = datasets.ImageFolder(
+	traindir,
+	transforms.Compose([
+	transforms.ToTensor(),
+	]))
+val_dataset = datasets.ImageFolder(
+	valdir,
+	transforms.Compose([
+	transforms.ToTensor(),
+	]))
+train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=1, shuffle=False,num_workers=8,drop_last=False)
+test_loader = torch.utils.data.DataLoader(val_dataset,batch_size=1, shuffle=False,num_workers=8,drop_last=False)
+#test_loader = torch.utils.data.DataLoader(cifar_test,batch_size=1, shuffle=False,num_workers=2,drop_last=False)
+def imshow(img,i,size):
 	npimg = img.numpy()
 	#print(npimg.shape)
 	npimg_size = npimg.size
 	npimg_shape = npimg.shape
-
-	if args.blur_std == 0:
-		gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-
+	gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
+	gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
+	gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
+	
+	'''	
+	npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std) + gaussian_noise_0.reshape(npimg_shape[1],npimg_shape[2])
+	npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std) + gaussian_noise_1.reshape(npimg_shape[1],npimg_shape[2])
+	npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std) + gaussian_noise_2.reshape(npimg_shape[1],npimg_shape[2])
+	
+	npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std)
+	npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std)
+	npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std)
+	'''
+	if not(args.gaussian_std == 0):
 		npimg[0] = npimg[0] + gaussian_noise_0.reshape(npimg_shape[1],npimg_shape[2])
 		npimg[1] = npimg[1] + gaussian_noise_1.reshape(npimg_shape[1],npimg_shape[2])
 		npimg[2] = npimg[2] + gaussian_noise_2.reshape(npimg_shape[1],npimg_shape[2])
-	elif args.gaussian_std == 0:
-		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std)
-		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std)
-		npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std)
-	else:	
-		gaussian_noise_0 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_1 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-		gaussian_noise_2 = np.random.normal(0,args.gaussian_std,npimg_shape[1]*npimg_shape[2])
-
-		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std) + gaussian_noise_0.reshape(npimg_shape[1],npimg_shape[2])
-		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std) + gaussian_noise_1.reshape(npimg_shape[1],npimg_shape[2])
-		npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std) + gaussian_noise_2.reshape(npimg_shape[1],npimg_shape[2])
-		
-		npimg[0] = scipy.ndimage.filters.gaussian_filter(npimg[0], args.blur_std)
-		npimg[1] = scipy.ndimage.filters.gaussian_filter(npimg[1], args.blur_std)
-		npimg[2] = scipy.ndimage.filters.gaussian_filter(npimg[2], args.blur_std)
 	
 	## add gaussian noise using numpy or scipy
-	str_end = len(dirty[folder_idx])-1
-	foldername = dirty[folder_idx][0:str_end]
-	if not os.path.isdir(foldername):
-		os.mkdir(foldername)
-		assert os.path.isdir(foldername), 'Error: no checkpoint directory found!'
-	scipy.misc.toimage(np.transpose(npimg, (1,2,0))).save(foldername+"/imagenet_gaussian_{}_blur_{}_train_{i}.png".format(args.gaussian_std,args.blur_std,i=batch_idx))
+	if args.val:
+		foldername = '/home/Imagenet2012/dirty/imagenet_gaussian_{}_blur_{}_val'.format(args.gaussian_std, args.blur_std)
+		if not os.path.isdir(foldername):
+			os.mkdir(foldername)
+			assert os.path.isdir(foldername), 'Error: no checkpoint directory found!'
+		scipy.misc.toimage(np.transpose(npimg, (1,2,0))).save(foldername+"/imagenet_gaussian_{}_blur_{}_val_{i}.png".format(args.gaussian_std,args.blur_std,i=i))
+	if args.train:
+		foldername = '/home/Imagenet2012/dirty/imagenet_gaussian_{}_blur_{}_train'.format(args.gaussian_std, args.blur_std)
+		if not os.path.isdir(foldername):
+			os.mkdir(foldername)
+			assert os.path.isdir(foldername), 'Error: no checkpoint directory found!'
+		scipy.misc.toimage(np.transpose(npimg, (1,2,0))).save(foldername+"/imagenet_gaussian_{}_blur_{}_train_{i}.png".format(args.gaussian_std,args.blur_std,i=i))
 	
+	progress_bar(batch_idx,size)
 
-
-for folder_idx in range(0,1000):
-	
-	str_end = len(clean[folder_idx])-1
-	filename = clean[folder_idx][0:str_end]
-	direc = os.path.join(filename)
-
-	dataset = datasets.ImageFolder(
-		direc,
-		transforms.Compose([
-			transforms.ToTensor(),
-		]))
-
-	loader = torch.utils.data.DataLoader(dataset,batch_size=1, shuffle=False,num_workers=2,drop_last=False)
-	#test_loader = torch.utils.data.DataLoader(cifar_test,batch_size=1, shuffle=False,num_workers=2,drop_last=False)
-
-	for batch_idx, (inputs, targets) in enumerate(loader):
-		imshow(torchvision.utils.make_grid(inputs), batch_idx, folder_idx)
-	
-	progress_bar(folder_idx, 1000);
-
+if args.val == 1:
+	for batch_idx, (inputs, targets) in enumerate(test_loader):
+		#if batch_idx < 10:
+		imshow(torchvision.utils.make_grid(inputs),batch_idx,len(test_loader)
+if args.train == 1:
+	for batch_idx, (inputs, targets) in enumerate(train_loader):
+		#if batch_idx < 10:
+		imshow(torchvision.utils.make_grid(inputs),batch_idx, len(train_loader)
 print('\n')
